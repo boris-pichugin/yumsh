5448c795813e7a7bc811727107e97f0863fb8e16
Борис Пичугин <boris.pichugin@gmail.com>
2024-02-03 11:15:33 +0300

    Урок 2024-01-20

--- Уроки/2023-2024 9к/2024-01-20/02.py
+++ Уроки/2023-2024 9к/2024-01-20/02.py
@@ -10,20 +10,21 @@ def main():
         print(f"{direction} {position}")
 
 
-def split_rectangle(n: int, m: int) -> Tuple[str, int]:
+def split_rectangle(n, m):
+    s = (n * m + 1) * n * m // 2
     min_direction = "V"
     min_position = 1
-    min_diff = 1 + (n * m + 1) * m * n // 2
+    min_diff = 1 + s
 
     # Вертикально
     if m > 1:
-        a = 1
-        b = 1 + (n - 1) * m
-        c = - (n * m + 1) * m // 2
+        a = n
+        b = n * (1 + (n - 1) * m)
+        c = -s
         start = solve(a, b, c)
         for x in (start, start + 1):
             if 0 <= x <= m:
-                diff = abs(n * (a * x * x + b * x + c))
+                diff = abs(a * x * x + b * x + c)
                 if diff < min_diff:
                     min_direction = "V"
                     min_position = x + 1
@@ -31,13 +32,13 @@ def split_rectangle(n: int, m: int) -> Tuple[str, int]:
 
     # Горизонтально
     if n > 1:
-        a = m
-        b = 1
-        c = - (n * m + 1) * n // 2
+        a = m * m
+        b = m
+        c = -s
         start = solve(a, b, c)
         for x in (start, start + 1):
             if 0 <= x <= n:
-                diff = abs(m * (a * x * x + b * x + c))
+                diff = abs(a * x * x + b * x + c)
                 if diff < min_diff:
                     min_direction = "H"
                     min_position = x + 1
@@ -48,17 +49,7 @@ def split_rectangle(n: int, m: int) -> Tuple[str, int]:
 
 def solve(a: int, b: int, c: int) -> int:
     x = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
-    x = math.floor(x)
-    f0 = a * x ** 2 + b * x + c
-    f1 = a * (x + 1) ** 2 + b * (x + 1) + c
-    while 0 < f0:
-        x = x - 1
-        f0 = a * x ** 2 + b * x + c
-    while f1 <= 0:
-        x = x + 1
-        f1 = a * (x + 1) ** 2 + b * (x + 1) + c
-
-    return x
+    return math.floor(x)
 
 
 main()
