ad9a7133d28c11427745c31a424d6571edb36a0e
Борис Пичугин <boris.pichugin@gmail.com>
2024-10-14 10:36:39 +0300

    Кружок 2024-11-14: Сокеты

--- Кружок/Проекты/05. Sockets/src/main/java/org/yumsh/club/Client.java
+++ Кружок/Проекты/05. Sockets/src/main/java/org/yumsh/club/Client.java
@@ -6,7 +6,8 @@ import java.nio.charset.StandardCharsets;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        try (Socket socket = new Socket("192.168.1.163", 5333)) {
+//        try (Socket socket = new Socket("192.168.1.163", 5333)) {
+        try (Socket socket = new Socket("127.0.0.1", 5333)) {
 
             OutputStream out = socket.getOutputStream();
             OutputStreamWriter writer = new OutputStreamWriter(out);
--- Кружок/Проекты/05. Sockets/src/main/java/org/yumsh/club/Server.java
+++ Кружок/Проекты/05. Sockets/src/main/java/org/yumsh/club/Server.java
@@ -7,20 +7,35 @@ import java.nio.charset.StandardCharsets;
 
 public class Server {
     public static void main(String[] args) {
-        try (ServerSocket server = new ServerSocket(5333, 5)) {
-            try (Socket socket = server.accept()) {
-                InputStream in = socket.getInputStream();
-                InputStreamReader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
-                BufferedReader br = new BufferedReader(reader);
-                String msg = br.readLine();
-                System.out.println("Сервер получил: " + msg);
-
-                OutputStream out = socket.getOutputStream();
-                OutputStreamWriter writer = new OutputStreamWriter(out);
-                writer.write(msg + '\n');
-                writer.flush();
+        try (ServerSocket server = new ServerSocket(5333, 3)) {
+            int i = 0;
+            while (true) {
+                Socket socket = server.accept();
+                Thread thread = new Thread(
+                        () -> handleClientSocket(socket),
+                        "MyThread-" + (++i)
+                );
+                thread.start();
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    private static void handleClientSocket(Socket socket) {
+        try(socket) {
+            Thread.sleep(10000L);
+            InputStream in = socket.getInputStream();
+            InputStreamReader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
+            BufferedReader br = new BufferedReader(reader);
+            String msg = br.readLine();
+            System.out.println("Сервер получил: " + msg);
+
+            OutputStream out = socket.getOutputStream();
+            OutputStreamWriter writer = new OutputStreamWriter(out);
+            writer.write(msg + '\n');
+            writer.flush();
+        } catch (final Exception e) {
             e.printStackTrace(System.err);
         }
     }
