7523e7629762e15882ac30e4cbae9de28703e0ac
Борис Пичугин <boris.pichugin@gmail.com>
2024-10-21 10:42:51 +0300

    Урок 2024-10-19: Сортировка

--- Уроки/2024-2025 10к/JavaProjects/lessons/src/main/java/org/yumsh/lesson02/Sort.java
+++ Уроки/2024-2025 10к/JavaProjects/lessons/src/main/java/org/yumsh/lesson02/Sort.java
@@ -4,7 +4,6 @@ package org.yumsh.lesson02;
  * Методы сортировки массивов.
  */
 public class Sort {
-
     /**
      * Отсортировать массив методом выбора.
      *
@@ -23,6 +22,50 @@ public class Sort {
                 double tmp = arr[i];
                 arr[i] = arr[minIdx];
                 arr[minIdx] = tmp;
+            }
+        }
+    }
+
+    /**
+     * Отсортировать массив методом вставки.
+     *
+     * @param arr массив, который будет отсортирован.
+     */
+    public static void sortByInsertion(double[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] <= arr[j - 1]) {
+                    double tmp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = tmp;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Сортировка бакетами.
+     *
+     * @param arr массив, который будет отсортирован.
+     */
+    public static void sortByBucket(int[] arr) {
+        int m = 0;
+        for (int x : arr) {
+            if (m < x) {
+                m = x;
+            }
+        }
+        int[] counts = new int[m + 1];
+        for (int x : arr) {
+            counts[x]++;
+        }
+        int i = 0;
+        for (int x = 0; x <= m; x++) {
+            int count = counts[x];
+            for (int k = 0; k < count; k++) {
+                arr[i++] = x;
             }
         }
     }
