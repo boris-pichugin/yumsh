1eff6e8fa1379788b5e019fa8f7fdf8e8836cfd2
Борис Пичугин <boris.pichugin@gmail.com>
2025-02-15 10:10:58 +0300

    Урок 2025-02-15: ArrayYList

--- Уроки/2024-2025 10к/JavaProjects/lessons/src/main/java/org/yumsh/collections/ArrayYList.java
+++ Уроки/2024-2025 10к/JavaProjects/lessons/src/main/java/org/yumsh/collections/ArrayYList.java
@@ -1,11 +1,11 @@
 package org.yumsh.collections;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class ArrayYList implements YList {
-
-    Object[] values = new Object[16];
-    int size;
+    private Object[] values = new Object[16];
+    private int size = 0;
 
     @Override
     public int size() {
@@ -14,7 +14,7 @@ public class ArrayYList implements YList {
 
     @Override
     public Object get(int i) {
-        if (i < 0 || size <= i) {
+        if (size <= i) {
             throw new ArrayIndexOutOfBoundsException(i);
         }
         return values[i];
@@ -22,7 +22,10 @@ public class ArrayYList implements YList {
 
     @Override
     public void set(int i, Object item) {
-
+        if (size <= i) {
+            throw new ArrayIndexOutOfBoundsException(i);
+        }
+        values[i] = item;
     }
 
     @Override
@@ -30,27 +33,57 @@ public class ArrayYList implements YList {
         if (size == values.length) {
             values = Arrays.copyOf(values, size * 2);
         }
-        values[size] = item;
-        size += 1;
+        values[size++] = item;
     }
 
     @Override
     public void insert(int i, Object item) {
-
+        if (size < i) {
+            throw new ArrayIndexOutOfBoundsException(i);
+        }
+        if (size == values.length) {
+            Object[] newValues = new Object[size * 2];
+            System.arraycopy(values, 0, newValues, 0, i);
+            System.arraycopy(values, i, newValues, i + 1, size - i);
+            values = newValues;
+        } else {
+            System.arraycopy(values, i, values, i + 1, size - i);
+        }
+        values[i] = item;
+        size += 1;
     }
 
     @Override
     public void remove(int i) {
-
+        if (size <= i) {
+            throw new ArrayIndexOutOfBoundsException(i);
+        }
+        size -= 1;
+        System.arraycopy(values, i + 1, values, i, size - i);
+        values[size] = null;
     }
 
     @Override
     public int indexOf(Object item) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(item, values[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public void removeAll(Object item) {
-
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            if (!Objects.equals(values[i], item)) {
+                values[j++] = values[i];
+            }
+        }
+        for (int i = j; i < size; i++) {
+            values[i] = null;
+        }
+        size = j;
     }
 }
