3505195fdcc94763dc98d2035a4fe79bec79b025
Борис Пичугин <boris.pichugin@gmail.com>
2025-02-22 09:28:23 +0300

    Урок 2025-02-22: YListTest

--- Уроки/2024-2025 10к/JavaProjects/lessons/src/test/java/org/yumsh/collections/YListTest.java
+++ Уроки/2024-2025 10к/JavaProjects/lessons/src/test/java/org/yumsh/collections/YListTest.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Test;
 public class YListTest {
 
     private static YList createList() {
-        return new ArrayYList();
+        return new DirectArrayYList();
+//        return new ArrayYList();
     }
 
     @Test
@@ -29,24 +30,22 @@ public class YListTest {
             list.insert(i, value);
             Assertions.assertSame(list.get(i), value);
         }
-        for (int i = 0; i < 10; i++) {
-            Assertions.assertEquals(list.get(i), i);
-        }
+        checkValues(list,
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+        );
+
         for (int i = 0; i < 10; i++) {
             Integer value = i + 10;
             int idx = 4 + i;
             list.insert(idx, value);
             Assertions.assertSame(list.get(idx), value);
         }
-        Assertions.assertEquals(20, list.size());
-        int[] values = {
+        checkValues(
+            list,
             0, 1, 2, 3,
             10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
             4, 5, 6, 7, 8, 9
-        };
-        for (int i = 0; i < values.length; i++) {
-            Assertions.assertEquals(values[i], list.get(i));
-        }
+        );
 
         try {
             list.insert(-1, 0);
@@ -58,6 +57,13 @@ public class YListTest {
             list.insert(list.size() + 1, 0);
             Assertions.fail();
         } catch (Exception ignored) {
+        }
+    }
+
+    private static void checkValues(YList list, Object... values) {
+        Assertions.assertEquals(values.length, list.size());
+        for (int i = 0; i < values.length; i++) {
+            Assertions.assertEquals(values[i], list.get(i));
         }
     }
 }
