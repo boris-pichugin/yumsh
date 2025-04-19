8e220015a2e9b682562af32b4b78427b1b6d922f
Борис Пичугин <boris.pichugin@gmail.com>
2025-04-19 12:16:05 +0300

    Урок 2025-04-19: YDeque

---
+++ Уроки/2024-2025 10к/JavaProjects/lessons/src/main/java/org/yumsh/collections/ArrayYDeque.java
@@ -0,0 +1,38 @@
+package org.yumsh.collections;
+
+public class ArrayYDeque implements YDeque {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void addFirst(Object value) {
+
+    }
+
+    @Override
+    public Object removeFirst() {
+        return null;
+    }
+
+    @Override
+    public Object firstElement() {
+        return null;
+    }
+
+    @Override
+    public void addLast(Object value) {
+
+    }
+
+    @Override
+    public Object removeLast() {
+        return null;
+    }
+
+    @Override
+    public Object lastElement() {
+        return null;
+    }
+}
---
+++ Уроки/2024-2025 10к/JavaProjects/lessons/src/main/java/org/yumsh/collections/YDeque.java
@@ -0,0 +1,68 @@
+package org.yumsh.collections;
+
+public interface YDeque extends YStack, YQueue {
+    /**
+     * @return число элементов в очереди.
+     */
+    int size();
+
+    /**
+     * @param value добавить элемент в начало.
+     */
+    void addFirst(Object value);
+
+    /**
+     * Удалить элемент из начала.
+     */
+    Object removeFirst();
+
+    /**
+     * @return элемент из начала.
+     */
+    Object firstElement();
+
+    /**
+     * @param value добавить элемент в конец.
+     */
+    void addLast(Object value);
+
+    /**
+     * Удалить элемент из конца.
+     */
+    Object removeLast();
+
+    /**
+     * @return элемент из конца.
+     */
+    Object lastElement();
+
+    @Override
+    default void push(Object value) {
+        addLast(value);
+    }
+
+    @Override
+    default Object pop() {
+        return removeLast();
+    }
+
+    @Override
+    default Object peek() {
+        return lastElement();
+    }
+
+    @Override
+    default void add(Object value) {
+        addLast(value);
+    }
+
+    @Override
+    default Object remove() {
+        return removeFirst();
+    }
+
+    @Override
+    default Object element() {
+        return firstElement();
+    }
+}
