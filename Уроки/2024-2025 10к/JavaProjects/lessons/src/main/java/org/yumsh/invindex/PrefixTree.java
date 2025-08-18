543f59e7f265a52e7b0dc1f9cca9f398b8d9c1ec
Boris Pichugin <boris.pichugin@gmail.com>
2025-08-18 15:06:04 +0300

    Лагерь 2025-08-18: PrefixTree

--- Уроки/2024-2025 10к/JavaProjects/lessons/src/main/java/org/yumsh/invindex/PrefixTree.java
+++ Уроки/2024-2025 10к/JavaProjects/lessons/src/main/java/org/yumsh/invindex/PrefixTree.java
@@ -1,28 +1,20 @@
 package org.yumsh.invindex;
 
-/**
- * Префиксное дерево.
- */
+/// Префиксное дерево.
 public interface PrefixTree {
-    /**
-     * @return число элементов в словаре.
-     */
+    /// @return число элементов в словаре.
     int size();
 
-    /**
-     * Присвоить данное значение данному ключу.
-     * <p>
-     * Если {@code value == null}, то пара {@code (key, value)}
-     * удаляется из словаря.
-     *
-     * @param key   ключ.
-     * @param value значение.
-     */
+    /// Присвоить данное значение данному ключу.
+    ///
+    /// Если `value == null`, то пара `(key, value)`
+    /// удаляется из словаря.
+    ///
+    /// @param key   ключ.
+    /// @param value значение.
     void put(String key, Object value);
 
-    /**
-     * @param key ключ.
-     * @return значение, сопоставленное ключу.
-     */
+    /// @param key ключ.
+    /// @return значение, сопоставленное ключу.
     Object get(String key);
 }
