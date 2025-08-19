353f8fa1d9c801f1ae84b9515ecd981b2f76f8c4
Boris Pichugin <boris.pichugin@gmail.com>
2025-08-19 15:39:04 +0300

    Лагерь 2025-08-18: StringLexEnumeratorImpl

---
+++ Уроки/2024-2025 10к/JavaProjects/lessons/src/main/java/org/yumsh/invindex/StringLexEnumeratorImpl.java
@@ -0,0 +1,110 @@
+package org.yumsh.invindex;
+
+import java.nio.charset.StandardCharsets;
+
+public class StringLexEnumeratorImpl implements StringLexEnumerator {
+    private final Node root = new Node();
+    private int maxBytesLen = 0;
+
+    @Override
+    public int size() {
+        return root.size;
+    }
+
+    @Override
+    public int put(String str) {
+        if (str == null) {
+            throw new IllegalArgumentException();
+        }
+        byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
+        if (maxBytesLen < bytes.length) {
+            maxBytesLen = bytes.length;
+        }
+        int idx = 0;
+        Node node = root;
+        for (byte b : bytes) {
+            int bi = b & 0xFF;
+            if (node.isEoS) {
+                idx += 1;
+            }
+            for (int i = 0; i < bi; i++) {
+                idx += node.children[i] == null ? 0 : node.children[i].size;
+            }
+            Node child = node.children[bi];
+            node = child == null ? (node.children[bi] = new Node()) : child;
+        }
+        if (!node.isEoS) {
+            node.isEoS = true;
+            node = root;
+            node.size += 1;
+            for (byte b : bytes) {
+                int bi = b & 0xFF;
+                node = node.children[bi];
+                node.size += 1;
+            }
+        }
+        return idx;
+    }
+
+    @Override
+    public int get(String str) {
+        if (str == null) {
+            throw new IllegalArgumentException();
+        }
+        byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
+        int idx = 0;
+        Node node = root;
+        for (byte b : bytes) {
+            int bi = b & 0xFF;
+            if (node.isEoS) {
+                idx += 1;
+            }
+            for (int i = 0; i < bi; i++) {
+                idx += node.children[i] == null ? 0 : node.children[i].size;
+            }
+            node = node.children[bi];
+            if (node == null) {
+                return idx;
+            }
+        }
+        return idx;
+    }
+
+    @Override
+    public String get(int i) {
+        if (size() <= i || i < 0) {
+            throw new IndexOutOfBoundsException(i);
+        }
+        byte[] bytes = new byte[maxBytesLen];
+        int pos = 0;
+
+        Node node = root;
+        while (true) {
+            if (node.isEoS) {
+                if (i == 0) {
+                    return new String(bytes, 0, pos, StandardCharsets.UTF_8);
+                }
+                i -= 1;
+            }
+            for (int b = 0; b < 256; b++) {
+                Node child = node.children[b];
+                if (child == null) {
+                    continue;
+                }
+                if (child.size <= i) {
+                    i -= child.size;
+                } else {
+                    node = child;
+                    bytes[pos++] = (byte) b;
+                    break;
+                }
+            }
+        }
+    }
+
+    private static final class Node {
+        private int size = 0;
+        private boolean isEoS = false;
+        private final Node[] children = new Node[256];
+    }
+}
