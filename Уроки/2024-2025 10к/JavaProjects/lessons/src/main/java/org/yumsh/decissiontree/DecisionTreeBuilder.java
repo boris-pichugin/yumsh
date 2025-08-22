3684462e00522a88d5186dd2db21b5b22adee07b
Boris Pichugin <boris.pichugin@gmail.com>
2025-08-22 15:20:24 +0300

    Лагерь 2025-08-22: DecisionTree

--- Уроки/2024-2025 10к/JavaProjects/lessons/src/main/java/org/yumsh/decissiontree/DecisionTreeBuilder.java
+++ Уроки/2024-2025 10к/JavaProjects/lessons/src/main/java/org/yumsh/decissiontree/DecisionTreeBuilder.java
@@ -8,6 +8,42 @@ public class DecisionTreeBuilder implements RegressionBuilder {
 
     @Override
     public Regression build() {
+        Node root = buildNode();
+        return root::compute;
+    }
+
+    private Node buildNode() {
         return null;
+    }
+
+    public static final class Node {
+        private final int idx;
+        private final double threshold;
+        private final double value;
+        private Node left = null;
+        private Node right = null;
+
+        public Node(int idx, double threshold) {
+            this.idx = idx;
+            this.threshold = threshold;
+            this.value = -1;
+        }
+
+        public Node(double value) {
+            this.idx = -1;
+            this.threshold = 0;
+            this.value = value;
+        }
+
+        double compute(double[] x) {
+            if (idx < 0) {
+                return value;
+            }
+            if (x[idx] < threshold) {
+                return left.compute(x);
+            } else {
+                return right.compute(x);
+            }
+        }
     }
 }
