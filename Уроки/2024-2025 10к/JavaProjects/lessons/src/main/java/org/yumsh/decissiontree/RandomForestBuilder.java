9e675a873808cdd5f7963aca9177ecdc923b7a2a
Boris Pichugin <boris.pichugin@gmail.com>
2025-08-23 15:43:20 +0300

    Лагерь 2025-08-23: RandomForestBuilder

--- Уроки/2024-2025 10к/JavaProjects/lessons/src/main/java/org/yumsh/decissiontree/RandomForestBuilder.java
+++ Уроки/2024-2025 10к/JavaProjects/lessons/src/main/java/org/yumsh/decissiontree/RandomForestBuilder.java
@@ -1,6 +1,19 @@
 package org.yumsh.decissiontree;
 
 public class RandomForestBuilder implements RegressionBuilder {
+
+    private final DecisionTreeBuilder decisionTreeBuilder;
+    private final int numTrees;
+
+    /// @param maxDepth         максимальная глубина дерева.
+    /// @param minSamplesInNode минимальное число обучающих образов в листе дерева.
+    /// @param subsamplePart    доля обучающих образов, которые будут использованы для построения дерева: `subsamplePart in [0;1]`.
+    /// @param numTrees         число деревьев в лесу.
+    public RandomForestBuilder(int maxDepth, int minSamplesInNode, double subsamplePart, int numTrees) {
+        this.decisionTreeBuilder = new DecisionTreeBuilder(maxDepth, minSamplesInNode, subsamplePart);
+        this.numTrees = numTrees;
+    }
+
     @Override
     public void add(double[] x, double y) {
 
