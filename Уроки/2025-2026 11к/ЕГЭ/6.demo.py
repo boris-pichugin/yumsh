066ba69df1f786aa32f40a81a5abce6e0494013f
Борис Пичугин <boris.pichugin@gmail.com>
2025-09-20 13:21:01 +0300

    2025-09-20 ЕГЭ

--- Уроки/2025-2026 11к/ЕГЭ/6.demo.py
+++ Уроки/2025-2026 11к/ЕГЭ/6.demo.py
@@ -1,14 +1,25 @@
 from turtle import *
 
-screensize(10000, 10000)
+screensize(1000, 1000)
 tracer(0)
 s = 20
-left(90)
 
 for i in range(2):
     forward(14 * s)
     left(270)
     back(12 * s)
+    right(90)
+up()
+forward(9 * s)
+right(90)
+back(7 * s)
+left(90)
+
+down()
+for i in range(2):
+    forward(13 * s)
+    right(90)
+    forward(6 * s)
     right(90)
 
 up()
